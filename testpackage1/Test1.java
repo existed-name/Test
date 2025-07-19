9459641242bd8f4c231ba91c840e17aefb1bcc12
春木秋水 <180740353+existed-name@users.noreply.github.com>
2025-07-19 20:18:18 +0800

    Update Test1.java
    添加 main 方法、startApplication 方法，其中 startApplication 方法用于输出 "Hello!"

--- testpackage1/Test1.java
+++ testpackage1/Test1.java
@@ -2,4 +2,11 @@ package com.github.existedname.testpackage1;
 
 
 public class Test1 {
+  public static int main( String[] args ){
+    startApplication();
+  }
+
+  public static void startApplication(){
+    System.out.print( "Hello!\n" );
+  }
 }
