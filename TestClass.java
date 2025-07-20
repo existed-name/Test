1914e17721b64e5361c84906c7f45befd2689554
春木秋水 <180740353+existed-name@users.noreply.github.com>
2025-07-20 16:07:47 +0800

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -1,4 +1,11 @@
-package com.github.existedname.testpackage1;
+package com.github.existedname.testpackage;
 
 public class TestClass {
+    public static void main( String[] args ){
+        startApplication( args );
+    }
+
+    public static void startApplication( String[] args ){
+        System.out.println( "Hello World!" );
+    }
 }
